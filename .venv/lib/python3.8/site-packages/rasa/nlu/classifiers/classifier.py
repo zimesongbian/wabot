1c26581e6ae2d26e13cd2ebb5b1c515383ae5dae
Songbian Zimé, PhD <90532587+zimesongbian@users.noreply.github.com>
2024-10-02 22:59:36 +0100

    Initial commit for resto_pizza

--- .venv/lib/python3.8/site-packages/rasa/nlu/classifiers/classifier.py
+++
@@ -1,5 +0,0 @@
-class IntentClassifier:
-    """An intent classifier."""
-
-    # TODO: move "add intent / rankings to message" functions here
-    pass
--- .venv/lib/python3.8/site-packages/rasa/nlu/featurizers/sparse_featurizer/sparse_featurizer.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-import scipy.sparse
-from rasa.nlu.featurizers.featurizer import Featurizer
-
-
-class SparseFeaturizer(Featurizer[scipy.sparse.spmatrix], ABC):
-    """Base class for all sparse featurizers."""
-
-    pass
